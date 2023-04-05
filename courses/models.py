7fbb8524e1d71edfb85ffb8344ce4603816095a3
Egor Batarchuk <egr311@ya.ru>
2023-04-05 17:18:44 +0300

    feat: update courses app models add __str__ methods add help_text to fields add blank flag to some fields

--- courses/models.py
+++ courses/models.py
@@ -16,39 +16,63 @@ class PersonMixin(models.Model):
     class Meta:
         abstract = True
 
-    first_name = models.CharField(max_length=32)
-    last_name = models.CharField(max_length=32)
-    patronymic_name = models.CharField(max_length=32, default='')
-    birth_date = models.DateField()
-    about_me = models.TextField(default='')
+    last_name = models.CharField(max_length=32, help_text='Фамилия')
+    first_name = models.CharField(max_length=32, help_text='Имя')
+    patronymic_name = models.CharField(
+        max_length=32, default='', blank=True, help_text='Отчество'
+    )
+    birth_date = models.DateField(help_text='Дата рождения')
+    about_me = models.TextField(
+        default='', blank=True, help_text='Расскажите о себе'
+    )
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name}'
 
 
 class Theme(models.Model):
-    theme_name = models.CharField(unique=True, max_length=64)
-    description = models.TextField()
+    theme_name = models.CharField(
+        unique=True, max_length=64, help_text='Название темы'
+    )
+    description = models.TextField(help_text='Описание темы')
+
+    def __str__(self):
+        return self.theme_name
 
 
 class Course(models.Model):  # стоит подумать о сущности Потока
-    course_name = models.CharField(unique=True, max_length=256)
-    about_course = models.TextField()
-    studied_themes = models.ManyToManyField(
-        Theme, related_name='courses_on_theme'
+    course_name = models.CharField(
+        unique=True, max_length=256, help_text='Название курса'
     )
+    about_course = models.TextField(help_text='Описание курса')
+    studied_themes = models.ManyToManyField(
+        Theme, related_name='courses_on_theme',
+        help_text='Темы, рассматриваемые в курсе.'
+    )
+
+    def __str__(self):
+        return self.course_name
 
 
 class Teacher(PersonMixin):
-    competences = models.ManyToManyField(Theme, related_name='theme_teachers')
+    competences = models.ManyToManyField(
+        Theme, related_name='theme_teachers',
+        help_text='Компетенции преподавателя.'
+    )
     taught_courses = models.ManyToManyField(
-        Course, related_name='course_teachers'
+        Course, related_name='course_teachers',
+        blank=True, help_text='Курсы, в которых преподаватель задействован.'
     )
 
 
 class Student(PersonMixin):
     studied_courses = models.ManyToManyField(
-        Course, related_name='students_passed'
+        Course, related_name='students_passed',
+        blank=True, help_text='Пройденные курсы.'
     )
-    courses_to_be_studied = models.ManyToManyField(
-        Course, related_name='students_current'
+    current_courses = models.ManyToManyField(
+        Course, related_name='students_current',
+        blank=True, help_text='Текущие курсы.'
     )
 
 # TODO добавить модели Расписания и Урока
